1705fe204f7d1dd0e5013afd79468d83a9d3d9b5
Kayn <kayn.pleiades@gmail.com>
2020-12-14 21:33:06 -0500

    Moving to second question

--- README.md
+++ README.md
@@ -23,6 +23,14 @@ My first goal: When the button is clicked, a prompt appears that prints to the b
 Then in next steps, I'll expand on this. 
 
 ### Commit 4: Password Length
-Next goal: Prompt asks for pasword length. If they provide a number that is not in the range, it will tell them and ask again. If number is in the range, it moves on. 
+Goal for this commit: Prompt asks for pasword length. If they provide a number that is not in the range, it will tell them and ask again. If number is in the range, it moves on. 
+
+### Commit 5: Moving to second question
+Goal for this commit: If a valid number is given, it moves on to the next prompt. Otherwise it lets them try again. 
+I have it direct you to the next prompt, but I am having trouble accessing the information that was inputed before. 
+Turns out my problem was that it was sending up to the next prompt before changing the object's properties
 
 ## credits
+
+Discussion on how to call a function within that same fuction https://stackoverflow.com/questions/2940862/javascript-how-to-restart-a-function-from-inside-it
+
--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var traits = {
+  ln:0, //length of password
+  lc:0, //lowercase characters
+  uc:0 //uppercase characters
+};
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -11,23 +17,31 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", passwordprompt);
+generateBtn.addEventListener("click", lengthprompt);
 
-// Prompt 1
-function passwordprompt() {
-  var prompt1 = prompt("Between 8 and 128 characters, how long would you like your password to be?", " ");
-  var passwordlength = parseInt(prompt1);
+// Prompt 1: Length Prompt
+function lengthprompt() {
+  var prompt1 = prompt("Between 8 and 128 characters, how long would you like your password to be?", "");
+  var passwordlength = parseInt(prompt1); // Get number from the string
 
-  if  (prompt1 == null || prompt1 == "") {
+  if  (prompt1 == null) { // If they click cancel
     alert("Cancelled");
   }
-  else if (passwordlength >= 8 && passwordlength <= 128) {
-    alert("Thank You");
+  else if (passwordlength >= 8 && passwordlength <= 128) { // If they enter a number in the range
+    traits.ln = passwordlength;
+    lowercaseprompt();
   }
-  else if (passwordlength < 8 || passwordlength > 128) {
-    alert("Please enter a number that is between 8 and 128");
+  else if (passwordlength < 8 || passwordlength > 128) { // If they enter a number outside of the range
+    alert("Please enter a number that is between 8 and 128"); // Tell them what they did wrong
+    lengthprompt(); // Call Function again
   }
-  else {
-    alert("I'm sorry, please try again.");
+  else { // If they enter anything else, such as text
+    alert("I'm sorry, something went wrong. Please try again."); // Tell them it didn't work
+    lengthprompt(); // Call Function again
   }
 }
+
+// Prompt 2: Lowercase Prompt
+  function lowercaseprompt(){
+    var prompt2 = prompt("Next Prompt goes here", "");
+  }
